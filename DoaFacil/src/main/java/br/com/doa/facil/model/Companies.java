ec77153f6712b7b597dcc4ea206ed8eaebd9dc54
CJ3019292 <CJ3019292@192.168.56.1>
2025-05-28 20:28:49 -0300

    Classes iniciais corrigidas

---
+++ DoaFacil/src/main/java/br/com/doa/facil/model/Companies.java
@@ -0,0 +1,104 @@
+package br.com.doa.facil.model;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "companies")
+public class Companies {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(max = 60)
+    private String socialName;
+
+    @NotNull
+    @Size(max = 60)
+    private String fantasyName;
+
+    @NotNull
+    @Email
+    @Size(max = 100)
+    private String email;
+
+    @NotNull
+    @Size(max = 11)
+    private String telephone;
+
+    @NotNull
+    private Boolean active;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSocialName() {
+        return socialName;
+    }
+
+    public void setSocialName(String socialName) {
+        this.socialName = socialName;
+    }
+
+    public String getFantasyName() {
+        return fantasyName;
+    }
+
+    public void setFantasyName(String fantasyName) {
+        this.fantasyName = fantasyName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        Companies other = (Companies) obj;
+        return Objects.equals(id, other.id);
+    }
+}
