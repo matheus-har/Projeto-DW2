e08712a4e1c443d0838780563f6aa092bc1a21c2
matheus-har <32464151+matheus-har@users.noreply.github.com>
2025-06-04 21:59:36 -0300

    Delete DoaFacil/src/main/resources/db/migration directory

--- DoaFacil/src/main/resources/db/migration/V04__criar_companies.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `companies` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `socialName` varchar(60) NOT NULL,
-  `fantasyName` varchar(60) NOT NULL,
-  `email`varchar(100) NOT NULL,
-  `telephone`varchar(11) NOT NULL,
-  `active` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
