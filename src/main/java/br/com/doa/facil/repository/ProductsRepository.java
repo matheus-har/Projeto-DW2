75b2cdf15ddfbb7305028d12fc54461048444323
aleantonuchi <152752246+aleantonuchi@users.noreply.github.com>
2025-06-26 19:32:50 -0300

    Delete src directory

--- src/main/java/br/com/doa/facil/repository/ProductsRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.doa.facil.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.doa.facil.model.Products;
-
-public interface ProductsRepository extends JpaRepository<Products ,Long>{
-
-}
