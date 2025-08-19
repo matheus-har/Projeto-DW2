7de26c7a29d813c5212f4ae1742e6fd535373116
PedroHenrique5785 <chaves.pedro@aluno.ifsp.edu.br>
2025-08-19 19:21:08 -0300

    Delete src directory

--- src/main/java/br/com/doa/facil/repository/CompaniesRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.doa.facil.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.doa.facil.model.Companies;
-
-public interface CompaniesRepository extends JpaRepository<Companies ,Long>{
-
-}
